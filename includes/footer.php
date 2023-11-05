52bced5663a54c403cf4c6b1078a515a28753b76
houssamyakhni <124498808+houssamyakhni@users.noreply.github.com>
2023-11-05 18:06:05 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Houssamyakhni2002@gmail.com |  | Contact Us: +961 81 849 624</b></span></div>
+</footer>
+    
